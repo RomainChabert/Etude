ad7943ae75ae30365cac5912572f3eff93eb3ac7
Romain Chabert <romain.chabert@outlook.com>
2021-12-10 18:33:19 +0100

    Code

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ if 'menu' not in st.session_state:
 
     gc = gspread.service_account_from_dict(credentials)
     sh = gc.open("Ouvertures")
-    worksheet = sh.Page
+    worksheet = sh.user_time
     worksheet.insert_row(temps_fin, 1)
 
 # Début de l'étude
