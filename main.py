67931418164726518b6e8db10affa07882a07241
Romain Chabert <romain.chabert@outlook.com>
2021-12-16 09:23:33 +0100

    Code

--- main.py
+++ main.py
@@ -619,7 +619,7 @@ elif st.session_state.menu == 2:
     # http://metadataconsulting.blogspot.com/2019/03/OneDrive-2019-Direct-File-Download-URL-Maker.html
 
     st.write("Cette seconde partie de l'étude, à effectuer sur un tableur, est constituée d'une série de cas pratiques sous Excel. [Appuyez sur ce lien] (https://onedrive.live.com/download?cid=E1CA44655646A7B5&resid=E1CA44655646A7B5%21261243&authkey=AIYTfMbO53hkK5o&em=2) pour télécharger le fichier Excel.")
-    st.write("Il s'agit d'un fichier au format .xlsm : il convient donc d'activer les macros afin de pouvoir l'utiliser, il y aura à cet effet en haut de l'écran un bandeau jaune avec un bouton 'Activer les macros'.")
+    st.write("Il s'agit d'un fichier au format .xlsm : il convient donc d'activer les macros afin de pouvoir l'utiliser. Il y aura à cet effet un bandeau jaune avec un bouton 'Activer les macros' en haut de l'écran.")
     st.write("_Attention : bien penser à enregistrer le fichier téléchargé avant de pouvoir le renvoyer ensuite._")
 
     st.markdown("Une fois terminé, merci de retourner le cas pratique par mail à l'adresse suivante : <a href='mailto:rchabert@deloitte.fr'>rchabert@deloitte.fr</a>.", unsafe_allow_html=True)
